f679c340b02017c4a4166aadc3659006b5a6a270
marina <roxy-lo@inbox.ru>
2022-07-07 20:21:13 +0300

    1st try

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"io"
 	"os"
 )
 
+type Arguments map[string]string
+
+type Users struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
 func Perform(args Arguments, writer io.Writer) error {
+	if args["fileName"] == "" {
+		return errors.New("-fileName flag has to be specified")
+	}
+	if args["operation"] == "" {
+		return errors.New("-operation flag has to be specified")
+	}
+	if args["item"] == "" {
+		return errors.New("-item flag has to be specified")
+	}
+	if args["id"] == "" {
+		return errors.New("-id flag has to be specified")
+	}
+	switch args["operation"] {
+	case "add":
+		return add(args, writer)
+	case "list":
+		return list(args, writer)
+	case "findById":
+		return findById(args, writer)
+	case "remove":
+		return remove(args, writer)
+	}
+	return nil
 }
 
 func main() {
@@ -13,4 +46,38 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func parseArgs() Arguments {
+	id := flag.String("id", "", "")
+	item := flag.String("item", "", "")
+	operation := flag.String("operation", "", "")
+	fileName := flag.String("fileName", "", "")
+	flag.Parse()
+	return Arguments{
+		"id":        *id,
+		"item":      *item,
+		"operation": *operation,
+		"fileName":  *fileName,
+	}
+}
+
+func add(args Arguments, writer io.Writer) error {
+
+	return nil
+}
+
+func list(args Arguments, writer io.Writer) error {
+
+	return nil
+}
+
+func findById(args Arguments, writer io.Writer) error {
+
+	return nil
+}
+
+func remove(args Arguments, writer io.Writer) error {
+
+	return nil
 }
